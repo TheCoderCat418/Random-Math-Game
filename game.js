1e110b919cde198a0ba79697dcecdfc0601f2aa4
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2023-09-07 09:58:39 -0500

    Day 3

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
-let diff, currentLevel, solution = 1
+let diff, currentLevel, solution = 1, operator = []
 function ondone(){
     if(solution === parseInt(document.getElementById("awn-box").value)){
+        document.getElementById("awn-box").value = ""
         let numbers = generateRandomNumber(diff,currentLevel)
         currentLevel++
         document.getElementById("problem").innerHTML = "What is " + numbers[0] + " + " +numbers[1]+"?"
@@ -12,15 +13,25 @@ function ondone(){
 function init(){
     let url = new URL(window.location.href);
     let param = url.searchParams;
-    document.getElementById("equation-types").innerHTML = param.get("equations");
+    operator = param.get("equations")
+     if(operator.indexOf("sub")){
+         document.getElementById("equation-types").innerHTML = "Subtraction!"
+     }else if(operator.indexOf("add")) {
+         document.getElementById("equation-types").innerHTML = "Addition!"
+     }
+
     diff = param.get("diff")
     currentLevel = 1
-
+    document.getElementById("awn-box").value = "1"
+    ondone()
+    document.getElementById("awn-box").value = ""
 
 }
 
-function generateRandomNumber(diff, level){
-    //if(diff === "easy"){
-    return {0: Math.trunc(Math.random() * level * 3), 1: Math.trunc(Math.random() * level * 3)};
-    //}
+function generateRandomNumber(diff){
+    if(diff === "easy"){
+        return [Math.round(Math.random() * 10 + 1), Math.trunc(Math.random() * 10 + 1)];
+    }else if(diff === "medium"){
+        return [Math.round(Math.random() * 100 + 1), Math.trunc(Math.random() * 100 + 1)];
+    }
 }
--- levelselect.js
+++ levelselect.js
@@ -1,9 +1,23 @@
+let diffMode, operators = []
 function colorDarken(levelColor, id){
     document.getElementById(id).style.backgroundColor = levelColor;
 }
-function goToLevel(levelName){
+function goToLevel(diff){
+    diffMode = diff
+    document.getElementById("diff").hidden = true
+    document.getElementById("opp").hidden = false
+}
+function setOp(id){
+    if(!operators.indexOf(id)){
+        operators.splice(id)
+    }else {
+        operators.push(id)
+    }
+    console.log(operators)
+}
+function gotogame(){
     let url = new URL("game.html", window.location.href);
-    url.searchParams.set("diff", levelName);
-    url.searchParams.set("equations", "addition");
+    url.searchParams.set("diff", diffMode);
+    url.searchParams.set("equations", operators);
     window.location.href = url.href;
 }
