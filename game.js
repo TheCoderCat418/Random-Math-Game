37d69111babc632f1bca8d9aa3e3b63a80eb983d
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2023-09-08 09:59:12 -0500

    Day 4

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
-let diff, currentLevel, solution = 1, operator = []
+let diff, currentLevel, solution = 1, operator = [], currentBadges = []
 function ondone(){
     if(solution === parseInt(document.getElementById("awn-box").value)){
         document.getElementById("awn-box").value = ""
         let numbers = generateRandomNumber(diff,currentLevel)
         currentLevel++
 
-        let sOperator = operator.at(Math.trunc(Math.random() * 2))
+        let sOperator = operator.at(Math.trunc(Math.random() * operator.length))
         console.log(sOperator)
         if(sOperator === "add"){
             solution = numbers[0]+numbers[1]
@@ -20,24 +20,47 @@ function ondone(){
             }
         }
     }
+    if(currentLevel === 5){
+        toggleGameScreen(false)
+        toggleBadge(true, "add5")
+    }
 
 
 }
-function showBadge(badgeName){
-
+function hideAllBadges(){
+    currentBadges.forEach(function (name){
+        document.getElementById(name).hidden = true
+    })
+}
+function toggleBadge(visi, badgeName){
+    hideAllBadges()
+    document.getElementById("badges").hidden = !visi
+    document.getElementById(badgeName).hidden = !visi
+}
+function toggleGameScreen(visi){
+    document.getElementById("game").hidden = !visi
+}
+function keepgoing(){
+    toggleGameScreen(true)
+    document.getElementById("badges").hidden = true
+    hideAllBadges()
+}
+function goback(){
+    let url = new URL("levelselect.html", window.location.href);
+    window.location.href = url.href;
 }
 function init(){
     let url = new URL(window.location.href);
     let param = url.searchParams;
+    document.getElementById("equation-types").innerHTML = ""
     operator = JSON.parse(param.get("equations"))
-     if(operator.indexOf("sub")){
-         document.getElementById("equation-types").innerHTML = "Subtraction!"
-     }else if(operator.indexOf("add")) {
-         document.getElementById("equation-types").innerHTML = "Addition!"
-     }
+    operator.forEach(function (value){
+        document.getElementById("equation-types").innerHTML = document.getElementById("equation-types").innerHTML + value + ", "
+    })
+
 
     diff = param.get("diff")
-    currentLevel = 1
+    currentLevel = -1
     document.getElementById("awn-box").value = "1"
     ondone()
     document.getElementById("awn-box").value = ""
