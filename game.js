73bfbd5c4751b9df830b419054851c4ae000f504
TheCoderCat418 <89421400+TheCoderCat418@users.noreply.github.com>
2023-09-11 09:59:47 -0500

    Day 5 | Due Friday!

--- game.js
+++ game.js
@@ -1,16 +1,17 @@
 let diff, currentLevel, solution = 1, operator = [], currentBadges = []
-function ondone(){
-    if(solution === parseInt(document.getElementById("awn-box").value)){
+
+function ondone() {
+    if (solution === parseInt(document.getElementById("awn-box").value)) {
         document.getElementById("awn-box").value = ""
-        let numbers = generateRandomNumber(diff,currentLevel)
+        let numbers = generateRandomNumber(diff, currentLevel)
         currentLevel++
 
         let sOperator = operator.at(Math.trunc(Math.random() * operator.length))
         console.log(sOperator)
-        if(sOperator === "add"){
-            solution = numbers[0]+numbers[1]
-            document.getElementById("problem").innerHTML = "What is " + numbers[0] + " + " +numbers[1]+"?"
-        }else {
+        if (sOperator === "add") {
+            solution = numbers[0] + numbers[1]
+            document.getElementById("problem").innerHTML = "What is " + numbers[0] + " + " + numbers[1] + "?"
+        } else {
             if (numbers[0] > numbers[1]) {
                 solution = numbers[0] - numbers[1]
                 document.getElementById("problem").innerHTML = "What is " + numbers[0] + " - " + numbers[1] + "?"
@@ -20,42 +21,38 @@ function ondone(){
             }
         }
     }
-    if(currentLevel === 5){
-        toggleGameScreen(false)
-        toggleBadge(true, "add5")
+    if (currentLevel === 5) {
+        solution = 0
+        document.getElementById("problem").innerHTML = "Congratulations on completing 5 levels!"
+        toggleBadge(true, "5")
+    }else if(currentLevel === 15){
+        document.getElementById("problem").innerHTML = "Congratulations on completing 15 levels!"
+        toggleBadge(true, "15cat")
     }
 
 
 }
-function hideAllBadges(){
-    currentBadges.forEach(function (name){
+
+function hideAllBadges() {
+    currentBadges.forEach(function (name) {
         document.getElementById(name).hidden = true
     })
 }
-function toggleBadge(visi, badgeName){
-    hideAllBadges()
-    document.getElementById("badges").hidden = !visi
+
+function toggleBadge(visi, badgeName) {
     document.getElementById(badgeName).hidden = !visi
+
 }
-function toggleGameScreen(visi){
-    document.getElementById("game").hidden = !visi
-}
-function keepgoing(){
-    toggleGameScreen(true)
-    document.getElementById("badges").hidden = true
-    hideAllBadges()
-}
-function goback(){
-    let url = new URL("levelselect.html", window.location.href);
-    window.location.href = url.href;
-}
-function init(){
+
+function init() {
     let url = new URL(window.location.href);
     let param = url.searchParams;
     document.getElementById("equation-types").innerHTML = ""
     operator = JSON.parse(param.get("equations"))
-    operator.forEach(function (value){
+    hideAllBadges()
+    operator.forEach(function (value) {
         document.getElementById("equation-types").innerHTML = document.getElementById("equation-types").innerHTML + value + ", "
+
     })
 
 
@@ -67,10 +64,19 @@ function init(){
 
 }
 
-function generateRandomNumber(diff){
-    if(diff === "easy"){
-        return [Math.round(Math.random() * 10 + 1), Math.trunc(Math.random() * 10 + 1)];
-    }else if(diff === "medium"){
+function generateRandomNumber(diff) {
+
+    if (diff === "easy") {
+        let num1 = Math.round(Math.random() * 10 * currentLevel/4 + 1)
+        if (num1 < 0) {
+            num1 = 1
+        }
+        let num2 = Math.round(Math.random() * 10 * currentLevel/4 + 1)
+        if (num2 < 0) {
+            num2 = 1
+        }
+        return [num1, num2];
+    } else if (diff === "medium") {
         return [Math.round(Math.random() * 100 + 1), Math.trunc(Math.random() * 100 + 1)];
     }
 }
